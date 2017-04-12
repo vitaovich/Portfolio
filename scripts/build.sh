cd38128a8f05be350afbd64c61f1233be9b39432
vitaovich <vitaliyalekhnovich@gmail.com>
2017-04-11 21:08:41 -0700

    updated travis to build deploy and install test

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir _site
+echo 'hello world' > _site/wtest.txt
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo "hello world"
+set -x
+echo 'Starting deployment...'
+if [[ $# -eq 0 ]]; then
+    echo 'No arguments supplied Testing deployment'
+    # Initialize a new git repo in _site, and push it to our server.
+    cd _site
+    git init
+
+    git remote add deploy "deploy@138.68.55.159:/var/repo/vitaovich/portfolio"
+    git config user.name "Travis CI"
+    git config user.email "vitaliyalekhnovich@gmail.com"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+    exit 1
+elif [[ $1 == "production" ]]; then
+    echo '-Production deployment selected-'
+
+elif [[ $1 == "development" ]]; then
+    echo '-Development deployment selected-'
+
+else
+    echo 'Invalid arguments supplied'
+fi
