0237175abd72d740b64e7d8d11460c016e28703e
vitaovich <vitaliyalekhnovich@gmail.com>
2017-04-11 23:17:28 -0700

    CI setup

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,7 @@ cp -r src/ bs-config.server.json package.json deploymentFolder
 cd deploymentFolder
 mv src/index.server.html src/index.html
 mv src/systemjs.config.server.js src/systemjs.config.js
-mv bs-config.server.json bs-config.server.json
+mv bs-config.server.json bs-config.json
+npm run build
+find . -type f -name '*.ts' -delete
 cd ..
