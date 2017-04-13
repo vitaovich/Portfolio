2d07791c732163f094af823fe51dbc9070676cff
vitaovich <vitaliyalekhnovich@gmail.com>
2017-04-13 10:57:16 -0700

    I created a reusable component. I learned how to make a component accept input. I learned to declare the required application directives in an Angular module. You listed the directives in the NgModule decorator's declarations array. I learned to bind a parent component to a child component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule // <--import the FormsModule before binding with [(ngModel)]
    ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
