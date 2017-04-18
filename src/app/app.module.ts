0be60f792195c7d4edf09ceee72ccaeff3f7a539
vitaovich <vitaliyalekhnovich@gmail.com>
2017-04-18 11:58:07 -0700

    I added the Angular router to navigate among different components. I learned how to create router links to represent navigation menu items. I used router link parameters to navigate to the details of the user-selected hero. I shared the HeroService among multiple components. I moved HTML and CSS out of the component file and into their own files. I added the uppercase pipe to format data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
+import { AppRoutingModule }  from './app-routing.module';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule // <--import the FormsModule before binding with [(ngModel)]
-   ],
+    FormsModule, // <--import the FormsModule before binding with [(ngModel)]
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [
+     AppComponent
+   ],
 })
+
+
+
 export class AppModule { }
