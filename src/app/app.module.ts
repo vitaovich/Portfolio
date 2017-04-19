e8077a2c94fcebafaebac18d1d01515aeb88a214
vitaovich <vitaliyalekhnovich@gmail.com>
2017-04-19 07:14:39 -0700

    I added the necessary dependencies to use HTTP in the app. I refactored HeroService to load heroes from a web API. I extended HeroService to support post(), put(), and delete() methods. I updated the components to allow adding, editing, and deleting of heroes. I configured an in-memory web API. I learned how to use Observables.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule }  from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
-import { AppRoutingModule }  from './app-routing.module';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 import { DashboardComponent } from './dashboard.component';
-
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule, // <--import the FormsModule before binding with [(ngModel)]
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule,
+
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent,
     DashboardComponent
   ],
   providers: [
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -9,12 +9,12 @@ import { HeroService } from './hero.service';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  heroes: Hero[] =[];
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1,5));
+      .then(heroes => this.heroes = heroes.slice( 1, 5 ));
   }
  }
