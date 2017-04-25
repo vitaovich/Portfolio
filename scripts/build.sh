ba84702b1041951703d070362c08b5dde38e43c9
vitaovich <vitaliyalekhnovich@gmail.com>
2017-04-25 08:54:21 -0700

    edit deploy build and typescript compiling

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 echo "Building Portfolio Web App"
-mkdir deploymentFolder
-cp -r src/ bs-config.server.json package.json deploymentFolder
-cd deploymentFolder
-mv src/index.server.html src/index.html
-mv src/systemjs.config.server.js src/systemjs.config.js
-mv bs-config.server.json bs-config.json
-find . -type f -name '*.ts' -delete
+OUT_PATH='./out'
+SRC_PATH='./src'
+
+echo "Removing previous build."
+rm -r $OUT_PATH/*
+
+echo "Copying source html, css, js, and ico files"
+cd $SRC_PATH
+find . -type f | grep .ico | xargs -i cp {} ../$OUT_PATH --parents
+find . -type f | grep .html | xargs -i cp {} ../$OUT_PATH --parents
+find . -type f | grep .css | xargs -i cp {} ../$OUT_PATH --parents
+find . -type f | grep .js | xargs -i cp {} ../$OUT_PATH --parents
 cd ..
+
+echo "Replacing references of node_modules with cdn https://unpkg.com"
+sed -i 's|node_modules|https://unpkg.com|g' $OUT_PATH/index.html
+echo "index.html replaced references"
+sed -i 's|node_modules|https://unpkg.com|g' $OUT_PATH/systemjs.config.js
+echo "systemjs.config.js replaced references"
+
+
+{ echo "COMPILING TypeScript USING"; tsc -v; }
+if [ "$1" == "-w" ]; then
+  tsc -w
+else
+  tsc --diagnostics --listEmittedFiles --listFiles
+  echo "FINISHED COMPILING."
+fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ echo 'Starting deployment...'
 if [[ $TRAVIS_BRANCH -eq 'master' ]]; then
     echo 'Deploying master branch'
     # Initialize a new git repo in _site, and push it to our server.
-    cd deploymentFolder
+    cd out
     git init
 
     git remote add deploy "deploy@138.68.55.159:/var/repo/vitaovich/portfolio"
