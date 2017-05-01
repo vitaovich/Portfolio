48210aabb75e18d515813666a688b37e32a5eaf8
vitaovich <vitaliyalekhnovich@gmail.com>
2017-05-01 11:06:09 -0700

    navigation bar styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,8 @@ import { AppInfoService } from './app-info.service';
 
 @Component({
   selector: 'my-app',
-  template: `
-
-    <nav>
-      <span>{{ name }}</span>
-      <a routerLink="/dashboard">Dashboard</a>
-      <a routerLink="/projects">Projects</a>
-      <a href="/assets/vitaliy-alekhnovich-resume.pdf">Resume</a>
-      <a href="https://github.com/vitaovich">Github</a>
-      <a href="https://www.linkedin.com/in/vitaliy-alekhnovich">LinkedIn</a>
-      <a href="https://travis-ci.org/vitaovich/Portfolio">
-        <img src="https://travis-ci.org/vitaovich/Portfolio.svg">
-      </a>
-      <button type="button"
-                    class="btn {{appinfo.isOn ? 'btn-success' : 'btn-danger'}}"
-                    (click)="appinfo.setInfoOn();">
-                    More Info: {{appinfo.isOn ? 'On' : 'Off'}}
-      </button>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: `./app.component.html`,
+  styleUrls: ['./app.component.css'],
   providers: [ LoggerService, AppInfoService ]
 })
 export class AppComponent  {
